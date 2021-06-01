fbb264677680f0c2e988f2840b700a531f545bb4
vhuntsman <vhuntsman@proton.me>
2021-06-01 21:02:00 +0800

    Adding initial code scaffolding and unit tests

---
+++ NcavScreener/Parser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace NcavScreener
+{
+    public class Parser
+    {
+        private readonly string context;
+
+        public Parser(string context)
+        {
+            this.context = context;
+        }
+
+        public List<string> ExtractPageLinks()
+        { 
+            var results = new List<string>();
+            // Define a regular expression for repeated words.
+            Regex td = new Regex(@"<td .* class=""body-table screener_pagination"".*?>(.*?)<\/td>", RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Singleline);
+            var tdContext = td.Matches(context)?[0].Groups[1].Value;
+            Regex links = new Regex(@"href=""(.*)""", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            var matches = links.Matches(tdContext);
+            foreach (Match match in matches)
+            {
+                results.Add(match.Groups[1].Value);
+            }
+            // results.RemoveAt(0);
+            results.RemoveAt(results.Count - 1);
+            return results;
+        }
+    }
+}
---
+++ NcavScreener/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using RestSharp;
+
+namespace NcavScreener
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var netCoreVer = System.Environment.Version; // 3.0.0
+            var runtimeVer = System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription; // .NET Core 3.0.0-preview4.19113.15
+            Console.WriteLine(runtimeVer);
+            var client = new RestClient("https://finviz.com/screener.ashx?v=111&f=fa_curratio_o1.5,fa_debteq_u0.5,fa_pb_low&ft=2&o=price");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("Cookie", "screenerUrl=screener.ashx%3Fv%3D111%26f%3Dfa_curratio_o1.5%2Cfa_debteq_u0.5%2Cfa_pb_low%26ft%3D2%26o%3Dprice");
+            IRestResponse response = client.Execute(request);
+            Console.WriteLine(response.Content);
+
+        }
+    }
+}
