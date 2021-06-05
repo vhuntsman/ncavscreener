16742a21720112e945b03e9606993d8cd85b8be1
timteh <timothytehsw@gmail.com>
2021-06-05 22:22:51 +0800

    Renamed pagination test file and added extract stock links method

--- NcavScreener/Program.cs
+++ NcavScreener/Program.cs
@@ -11,7 +11,7 @@ namespace NcavScreener
             var netCoreVer = System.Environment.Version; // 3.0.0
             var runtimeVer = System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription; // .NET Core 3.0.0-preview4.19113.15
             Console.WriteLine(runtimeVer);
-            var client = new RestClient("https://finviz.com/screener.ashx?v=111&f=fa_curratio_o1.5,fa_debteq_u0.5,fa_pb_low&ft=2&o=price");
+            var client = new RestClient("https://finviz.com/screener.ashx?v=111&f=fa_curratio_o1.5,fa_debteq_u0.5,fa_pb_low&ft=2&o=price"); // TODO: make criteria configurable in console or use default values
             client.Timeout = -1;
             var request = new RestRequest(Method.GET);
             request.AddHeader("Cookie", "screenerUrl=screener.ashx%3Fv%3D111%26f%3Dfa_curratio_o1.5%2Cfa_debteq_u0.5%2Cfa_pb_low%26ft%3D2%26o%3Dprice");
