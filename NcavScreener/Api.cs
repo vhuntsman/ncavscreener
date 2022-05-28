6585fb06adc49f2d9463d9f4e5858f498c0bfea3
vhuntsman <vhuntsman@proton.me>
2022-05-29 01:26:37 +0800

    Fixed breaking change - added user-agent and accept headers

--- NcavScreener/Api.cs
+++ NcavScreener/Api.cs
@@ -8,13 +8,13 @@ namespace NcavScreener
     public class Api
     {
         private RestResponseCookie _sessionCookie = new RestResponseCookie() { Name = "foo", Value = "bar" };
-        private RestRequest _request;
 
         public string GetAuthResponse(IRestClient client)
         {
             client.Timeout = -1;
-            _request = new RestRequest(Method.GET);
-            IRestResponse response = client.Execute(_request);
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
+            IRestResponse response = client.Execute(request);
             try
             {
                 _sessionCookie = response?.Cookies?[0];
@@ -27,11 +27,11 @@ namespace NcavScreener
         {
             client.Timeout = -1;
             var request = new RestRequest(Method.POST);
-            request.AddHeader("Content-Type", "text/plain");
+            request.AddHeader("Content-Type", "application/json");
             request.AddCookie(_sessionCookie.Name, _sessionCookie.Value);
             var queryOffset = 0;
             var body = @"{""size"":250,""offset"":" + queryOffset + @",""sortField"":""intradayprice"",""sortType"":""asc"",""quoteType"":""EQUITY"",""topOperator"":""AND"",""query"":{""operator"":""AND"",""operands"":[{""operator"":""or"",""operands"":[{""operator"":""EQ"",""operands"":[""region"",""us""]},{""operator"":""EQ"",""operands"":[""region"",""hk""]},{""operator"":""EQ"",""operands"":[""region"",""sg""]},{""operator"":""EQ"",""operands"":[""region"",""ca""]},{""operator"":""EQ"",""operands"":[""region"",""au""]},{""operator"":""EQ"",""operands"":[""region"",""jp""]},{""operator"":""EQ"",""operands"":[""region"",""gb""]},{""operator"":""EQ"",""operands"":[""region"",""fr""]},{""operator"":""EQ"",""operands"":[""region"",""fr""]},{""operator"":""EQ"",""operands"":[""region"",""de""]},{""operator"":""EQ"",""operands"":[""region"",""ch""]},{""operator"":""EQ"",""operands"":[""region"",""nl""]},{""operator"":""EQ"",""operands"":[""region"",""be""]},{""operator"":""EQ"",""operands"":[""region"",""lu""]},{""operator"":""EQ"",""operands"":[""region"",""at""]},{""operator"":""EQ"",""operands"":[""region"",""cz""]},{""operator"":""EQ"",""operands"":[""region"",""fi""]},{""operator"":""EQ"",""operands"":[""region"",""hu""]},{""operator"":""EQ"",""operands"":[""region"",""pl""]},{""operator"":""EQ"",""operands"":[""region"",""ru""]},{""operator"":""EQ"",""operands"":[""region"",""gr""]},{""operator"":""EQ"",""operands"":[""region"",""it""]},{""operator"":""EQ"",""operands"":[""region"",""pt""]},{""operator"":""EQ"",""operands"":[""region"",""es""]}]},{""operator"":""lt"",""operands"":[""totaldebtequity.lasttwelvemonths"",25]},{""operator"":""or"",""operands"":[{""operator"":""EQ"",""operands"":[""sector"",""Consumer Defensive""]},{""operator"":""EQ"",""operands"":[""sector"",""Utilities""]},{""operator"":""EQ"",""operands"":[""sector"",""Technology""]},{""operator"":""EQ"",""operands"":[""sector"",""Consumer Cyclical""]},{""operator"":""EQ"",""operands"":[""sector"",""Communication Services""]},{""operator"":""EQ"",""operands"":[""sector"",""Healthcare""]},{""operator"":""EQ"",""operands"":[""sector"",""Industrials""]},{""operator"":""EQ"",""operands"":[""sector"",""Energy""]}]},{""operator"":""lt"",""operands"":[""lastclosepricebookvalue.lasttwelvemonths"",1]}]},""userId"":"""",""userIdType"":""guid""}";
-            request.AddParameter("text/plain", body, ParameterType.RequestBody);
+            request.AddParameter("application/json", body, ParameterType.RequestBody);
             IRestResponse response = client.Execute(request);
             dynamic stocks = JsonConvert.DeserializeObject(response.Content);
             // Get the count of stocks
@@ -44,13 +44,13 @@ namespace NcavScreener
             while (queryOffset < stockCount)
             {
                 request.Parameters.Clear();
-                request.AddHeader("Content-Type", "text/plain");
+                request.AddHeader("Content-Type", "application/json");
                 if (_sessionCookie != null)
                 {
                     request.AddCookie(_sessionCookie.Name, _sessionCookie.Value);
                 }
                 body = @"{""size"":250,""offset"":" + queryOffset + @",""sortField"":""intradayprice"",""sortType"":""asc"",""quoteType"":""EQUITY"",""topOperator"":""AND"",""query"":{""operator"":""AND"",""operands"":[{""operator"":""or"",""operands"":[{""operator"":""EQ"",""operands"":[""region"",""us""]},{""operator"":""EQ"",""operands"":[""region"",""hk""]},{""operator"":""EQ"",""operands"":[""region"",""sg""]},{""operator"":""EQ"",""operands"":[""region"",""ca""]},{""operator"":""EQ"",""operands"":[""region"",""au""]},{""operator"":""EQ"",""operands"":[""region"",""jp""]},{""operator"":""EQ"",""operands"":[""region"",""gb""]},{""operator"":""EQ"",""operands"":[""region"",""fr""]},{""operator"":""EQ"",""operands"":[""region"",""fr""]},{""operator"":""EQ"",""operands"":[""region"",""de""]},{""operator"":""EQ"",""operands"":[""region"",""ch""]},{""operator"":""EQ"",""operands"":[""region"",""nl""]},{""operator"":""EQ"",""operands"":[""region"",""be""]},{""operator"":""EQ"",""operands"":[""region"",""lu""]},{""operator"":""EQ"",""operands"":[""region"",""at""]},{""operator"":""EQ"",""operands"":[""region"",""cz""]},{""operator"":""EQ"",""operands"":[""region"",""fi""]},{""operator"":""EQ"",""operands"":[""region"",""hu""]},{""operator"":""EQ"",""operands"":[""region"",""pl""]},{""operator"":""EQ"",""operands"":[""region"",""ru""]},{""operator"":""EQ"",""operands"":[""region"",""gr""]},{""operator"":""EQ"",""operands"":[""region"",""it""]},{""operator"":""EQ"",""operands"":[""region"",""pt""]},{""operator"":""EQ"",""operands"":[""region"",""es""]}]},{""operator"":""lt"",""operands"":[""totaldebtequity.lasttwelvemonths"",25]},{""operator"":""or"",""operands"":[{""operator"":""EQ"",""operands"":[""sector"",""Consumer Defensive""]},{""operator"":""EQ"",""operands"":[""sector"",""Utilities""]},{""operator"":""EQ"",""operands"":[""sector"",""Technology""]},{""operator"":""EQ"",""operands"":[""sector"",""Consumer Cyclical""]},{""operator"":""EQ"",""operands"":[""sector"",""Communication Services""]},{""operator"":""EQ"",""operands"":[""sector"",""Healthcare""]},{""operator"":""EQ"",""operands"":[""sector"",""Industrials""]},{""operator"":""EQ"",""operands"":[""sector"",""Energy""]}]},{""operator"":""lt"",""operands"":[""lastclosepricebookvalue.lasttwelvemonths"",1]}]},""userId"":"""",""userIdType"":""guid""}";
-                request.AddParameter("text/plain", body, ParameterType.RequestBody);
+                request.AddParameter("application/json", body, ParameterType.RequestBody);
                 IRestResponse loopResponse = client.Execute(request);
                 dynamic loopStocks = JsonConvert.DeserializeObject(loopResponse.Content);
                 foreach (var quote in loopStocks["finance"]["result"][0]["quotes"])
