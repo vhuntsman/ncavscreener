7d6f1442d27f100dc0f568cce5ac289f319eee90
timteh <timothytehsw@gmail.com>
2023-04-01 00:45:38 +0800

    Fix extract crumb regex as crumbstore is no longer available

--- NcavScreener/Parser.cs
+++ NcavScreener/Parser.cs
@@ -10,7 +10,7 @@ namespace NcavScreener
         public string ExtractCrumb(string context)
         {
             // Pattern for crumb
-            Regex re = new Regex(@"""CrumbStore"":{""crumb"":""(.*?)""}", RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Singleline);
+            Regex re = new Regex(@"""user"":{""crumb"":""(.*?)"".*?}", RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Singleline);
             var matches = re.Matches(context);
             return Regex.Unescape(matches[0].Groups[1].Value);
         }
